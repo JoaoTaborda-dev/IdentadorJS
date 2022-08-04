865b7cf2abac5e00d086b06ee8dec7bd3565a05d
João Campos <joaog.taborda.campos@gmail.com>
2022-08-04 18:51:32 -0300

    Refatorando o post de livros

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -6,6 +6,25 @@ class LivroController {
       res.status(200).json(livros)
     })
   }
+
+  static cadastrarLivro = (req, res) => {
+    let livro = new livros(req.body)
+
+    livro.save(err => {
+      if (err) {
+        res
+          .status(500)
+          .send({ message: `${err.message} - falha ao cadastrar livro.` })
+      } else {
+        res.status(201).send(livro.toJSON())
+      }
+    })
+  }
+
+  // static listaLivro = (''(req, res) => {
+  //   let index = buscaLivros(req.params.id)
+  //   res.json(livros[index])
+  // })
 }
 
 export default LivroController
